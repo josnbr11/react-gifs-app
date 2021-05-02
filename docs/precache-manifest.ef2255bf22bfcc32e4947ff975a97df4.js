3855bfb4a296134b7d32b99862c74cf419f28339
Jose Sanabria <mljoses@gmail.com>
2021-05-02 09:04:25 -0400

    add docs

---
+++ docs/precache-manifest.ef2255bf22bfcc32e4947ff975a97df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce26a504bccc656682f4d313601ff28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "049e7cee21220a074825",
+    "url": "/static/css/main.e7a2418d.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049e7cee21220a074825",
+    "url": "/static/js/main.cc4afc95.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
